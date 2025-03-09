bbcfd8ce3ba54f1456aa0b105ffbcaeec2bc20aa
markbuild <ismk@qq.com>
2025-03-09 22:33:11 +0800

    ud

---
+++ tmux/tmux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+tmux new-session -s mkSession -n todo -d "vim ~/code/lab/todolist"
+tmux new-window -n fileExplore -c ~/
+
+tmux new-window -n markbuild -c ~/code/markbuild/ "vim README.md"
+tmux split-window -h -c ~/code/markbuild/api
+tmux send-keys -t mkSession 'git status' C-m
+
+tmux attach -t mkSession
+
+#tmux kill-session -t mkSession
