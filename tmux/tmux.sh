3baf837033ccd67ae30373dd231377b1176d6d9d
markbuild <ismk@qq.com>
2026-01-15 23:47:32 +0800

    ud

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-start ~/k.ahk # windows 下的AutoHotKey
+# start ~/k.ahk # windows 下的AutoHotKey
 
 rm -rf ~/.vim/swap/*
 
@@ -9,16 +9,16 @@ tmux new-session -s mkSession -n fast -d
 if [ -f ~/.vim/session_fast.vim ]; then
   tmux send-keys -t mkSession 'vim -S ~/.vim/session_fast.vim' C-m
 else
-  tmux send-keys -t mkSession 'vim S ~/code/markbuild/h/fast' C-m
+  tmux send-keys -t mkSession 'vim ~/code/markbuild/h/fast' C-m
 fi
 
 tmux new-window -n fileExplore -c ~/
 
-tmux new-window -n markbuild -c ~/code/markbuild/
-tmux send-keys -t mkSession 'vim S ~/code/markbuild/' C-m
-tmux split-window -h -c ~/code/markbuild/
-tmux send-keys -t mkSession 'npm run serve' C-m
-tmux split-window -v -c ~/code/markbuild/
+tmux new-window -n api -c ~/code/markbuild/api
+tmux send-keys -t mkSession 'vim ~/code/markbuild/api/README.txt' C-m
+tmux split-window -h -c ~/code/markbuild/api
+#tmux send-keys -t mkSession 'npm run serve' C-m
+tmux split-window -v -c ~/code/markbuild/api
 tmux send-keys -t mkSession 'gs && cls' C-m
 
 tmux attach -t mkSession
